d57a85ea1e83ee7bac511a8a7800bc6996ddbef0
zengwei shao <zwshao@thoughtworks.com>
2016-11-20 15:06:46 +0800

    register the service to server

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 public class Application {
 
     @Value("${name}")
